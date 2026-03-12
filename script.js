3ef132a34b1308d33da64070836f0d8519df6a4d
Evan Longwood <24954397+AngryGrizzlyBear@users.noreply.github.com>
2026-03-12 11:40:00 -0700

    Added Javascript for scrolling and animations.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('#hamburger')
 const navList = document.querySelector('ul')
 const hamburgerItems = document.querySelectorAll('nav a')
+const target = document.querySelectorAll('.fade-up')
 
 hamburger.addEventListener("click", () => {
     navList.classList.toggle('open')
@@ -15,3 +16,34 @@ hamburgerItems.forEach((item) => {
         hamburger.setAttribute('aria-expanded', 'false');
     })
 })
+
+
+
+
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+}
+
+
+const observer = new IntersectionObserver(handleIntersection, options); 
+    
+
+
+target.forEach((el) => {
+        observer.observe(el)
+    })
+
+
